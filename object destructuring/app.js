a9744484e46fb1ffcb68218b61b60cf1bbfafe52
Yusuf <usuf7hahin@gmail.com>
2024-08-18 00:48:00 +0600

    object destructuring on process

--- object destructuring/app.js
+++ object destructuring/app.js
@@ -31,8 +31,27 @@ const {
   zip,
   siblings: { sister: favoriteSibling },
 } = bob;
-
 // console.log(firstName, lastName, city, zip, favoriteSibling);
+
+//! ============
+//? how obj destructuring work :-
+//! ============
+
+const myProfile = {
+  name: "Yushah",
+  job: "web developer",
+  address: {
+    current: "chittagong",
+    permanent: "noakhali",
+  },
+};
+
+const pAddress = myProfile.address.permanent;
+console.log(pAddress);
+const {
+  address: { current: cAddress },
+} = myProfile;
+console.log(cAddress);
 
 //* desturtuing function peremater :-
 function printPerson({ first, last, city, siblings: { sister } }) {
@@ -41,3 +60,51 @@ function printPerson({ first, last, city, siblings: { sister } }) {
 }
 
 printPerson(bob);
+
+//! ============
+//? obj destructuring in details
+//! ============
+
+//* create varible
+const name = "yusuf";
+const age = [
+  {
+    myAge: 27,
+    nazAge: 25,
+  },
+];
+const currentAddress = {
+  thana: "kotoali",
+  district: "ctg",
+  river: {
+    fRev: "podma",
+    sRev: "megna",
+  },
+};
+const homeDistrict = "noakhali";
+
+//* addEverything in obj
+const myBio = {
+  // every varible hold value inside it
+  name,
+  age,
+  currentAddress,
+  homeDistrict,
+};
+
+//* destructering obj
+const pickAge = myBio.age[0];
+const { myAge, nazAge } = pickAge;
+const {
+  currentAddress: { thana, district },
+} = myBio;
+const {
+  currentAddress: {
+    river: { fRev: p, sRev: m },
+  },
+} = myBio;
+
+// console.log(myBio);
+// console.log(thana, district);
+// console.log(myAge, nazAge);
+// console.log(p, m);
