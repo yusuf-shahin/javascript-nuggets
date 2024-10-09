6f86c381b07fc22ee6af5ab22f7869f0205fa09e
Yusuf <usuf7hahin@gmail.com>
2024-10-09 22:28:38 +0600

    something is change

--- conditional satement/app.js
+++ conditional satement/app.js
@@ -1,4 +1,13 @@
-//!==========
+// let chakePoint = "string" ;
+// chakePoint ---> true
+// let chakePoint = "" ;
+// chakePoint ---> false
+// but
+// !chakePoint ---> true
+
+//! ============
+//? simple example
+//! ============
 
 // const chaker = false
 
@@ -18,11 +27,11 @@
 
 //!==========
 
-const chaker = false
+// const chaker = false
 
-if (!chaker) {
-  console.log("chake is pass")
-} else console.log("chake is fail")
+// if (!chaker) {
+//   console.log("chake is pass")
+// } else console.log("chake is fail")
 //# chake is pass
 
 //!==========
@@ -42,3 +51,18 @@ if (!chaker) {
 //   console.log("chake is pass")
 // } else console.log("chake is fail")
 // //# chake is fail
+
+//! ===========
+//? Conditional function
+//! ===========
+
+// Empty string chacker
+
+// const stringChaker = (str) => {
+//   str ? console.log(str) : console.log("The string is empty")
+// }
+// console.log(stringChaker("hello world"))
+
+//? show the password for three second
+
+// let chaker = false
